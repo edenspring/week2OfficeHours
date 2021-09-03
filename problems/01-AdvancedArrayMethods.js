bcb9c496c9f4736e13b50381d79d8c5824d37e9b
Bill Adams <vikingbill@gmail.com>
2021-09-03 13:11:40 -0400

    turns out I'm bad at spelling and syntax

--- problems/01-AdvancedArrayMethods.js
+++ problems/01-AdvancedArrayMethods.js
@@ -5,11 +5,11 @@ that accepts an array and a callback and returns a new array. This new array
 should hold the results of passing each element through the callback.
 
 const arr1 = [1,2,3,4]
-const doubler = (num) => return num * 2
+const doubler = (num) => num * 2
 console.log(advance(arr1, doubler)) // [2,4,6,8]
 
 const arr2 = ["Hi", "there", "let's", "use", "strings"]
-const angryGoose = (word) return word + " " + "HONK!"
+const angryGoose = (word) => word + " " + "HONK!"
 console.log(advance(arr2, angryGoose)) // ["Hi HONK!",
                                            "there HONK!",
                                            "let's HONK!",
