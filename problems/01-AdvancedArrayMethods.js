013faccb24bdd7b8b51b47dec633edcbc7fba057
Bill Adams <vikingbill@gmail.com>
2021-09-03 12:26:25 -0400

    added problem 02

---
+++ problems/01-AdvancedArrayMethods.js
@@ -0,0 +1,39 @@
+/**********************************************************************
+
+Using an advanced array method of your choice, write a function called
+that advance thataccepts an array and a callback and returns a new array.
+This new array should hold the results of passing each element through the
+callback.
+
+const arr1 = [1,2,3,4]
+const doubler = (num) => return num * 2
+console.log(advance(arr1, doubler)) // [2,4,6,8]
+
+const arr2 = ["Hi", "there", "let's", "use", "strings"]
+const angryGoose = (word) return word + " " + "HONK!"
+console.log(advance(arr2, angryGoose)) // ["Hi HONK!", "there HONK!", "let's HONK!", "use HONK!", "strings HONK!"]
+
+
+ *********************************************************************/
+
+// const advance = (arr, cb) => arr.map(cb) // .map solve
+
+
+// const advance = (arr, cb) => arr.reduce((accum, ele) => { // .reduce solve
+//   accum.push(cb(ele))
+//   return accum;
+// }, [])
+
+// const advance = (arr, cb) => { //.forEach solve
+//   const result = []
+//   arr.forEach(ele => result.push(cb(ele)))
+//   return result;
+// }
+
+const arr1 = [1,2,3,4]
+const doubler = (num) => num * 2
+console.log(advance(arr1, doubler)) // [2,4,6,8]
+
+const arr2 = ["Hi", "there", "let's", "use", "strings"]
+const angryGoose = (word) => word + " " + "HONK!"
+console.log(advance(arr2, angryGoose)) // ["Hi HONK!", "there HONK!", "let's HONK!", "use HONK!", "strings HONK!"]
---
+++ problems/02-ObjectsPractice.js
@@ -0,0 +1,89 @@
+/**********************************************************************
+
+Let's explore objects!
+
+We'll store some information about pets in objects. Given an array of
+those objects, write a function called fuzzyFinder that returns the total
+count of names that include the word "fuzzy"
+
+For example:
+const petArray = [
+  {
+    "name": "Fuzzypants",
+   "owner" : "Dave",
+   "type" : "cat"
+  },
+  {
+    "name" : "The Lady Fuzzeralla of York",
+    "owner" : "Rebecca",
+    "type" : "cat"
+  },
+  {
+    "name": "Fuzzywuzzy",
+    "owner": "Audrey",
+    "type": "dog"
+  },
+  {
+    "name": "More fishy than fuzzy",
+    "owner": "Ash",
+    "type" : "fish"
+  }
+]
+
+console.log(fuzzyFinder(petArray)) // 3
+
+!!HINT!!
+You may want to use .toLowerCase() or toUpperCase() when testing strings
+
+  *********************************************************************/
+
+const petArray = [
+  {
+    "name": "Fuzzypants", //1
+   "owner" : "Dave",
+   "type" : "cat"
+  },
+  {
+    "name" : "The Lady Fuzzeralla of York",
+    "owner" : "Rebecca",
+    "type" : "cat"
+  },
+  {
+    "name": "Fuzzywuzzy", //2
+    "owner": "Audrey",
+    "type": "dog"
+  },
+  {
+    "name": "More fishy than fuzzy", //3
+    "owner": "Ash",
+    "type" : "fish"
+  }
+]
+
+// function fuzzyFinder(arr) { //reduce solve
+//   return arr.reduce((accum, ele) => {
+//     if (ele.name.toLowerCase().includes("fuzzy")) accum++
+//     return accum
+//   }, 0)
+// }
+
+// const fuzzyFinder = (arr) => { //forEach solve
+//   let count = 0
+//   arr.forEach(ele => {
+//     const name = ele.name
+//     if (name.toLowerCase().includes("fuzzy")) count++
+//   })
+//   return count
+// }
+
+// const fuzzyFinder = function(arr) { //filter solve
+//   return arr.filter(ele => {
+//     const name = ele.name.toUpperCase();
+//     if (name.includes("FUZZY")) return true
+//     else return false;
+//   }).length
+// }
+
+
+
+console.log(fuzzyFinder(petArray)) // 3
