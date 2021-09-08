20a58c3c3ae363d9b5edd7b04e825da4e17f6b97
Bill Adams <vikingbill@gmail.com>
2021-09-08 12:25:33 -0400

    ssh test

--- problems/01-AdvancedArrayMethods.js
+++ problems/01-AdvancedArrayMethods.js
@@ -19,6 +19,12 @@ console.log(advance(arr2, angryGoose)) // ["Hi HONK!",
  *********************************************************************/
 
 //Your code here
+let advance = (array, callback) => {
+  let result = array.map(function(ele){
+    return callback(ele);
+  })
+  return result;
+}
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
